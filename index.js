9723098b0675e5f0b78c7ece6f443e5dbd628bd2
Md.Riadul Islam Riad <kazuha3242@gmail.com>
2026-03-05 02:44:05 +0600

    Stripe payment api gateway Added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 // =============== Initial Ports and Connections =============================
 const app = express();
@@ -37,48 +38,77 @@ async function run() {
     await client.connect();
 
     // ========================== Databases & all Collection here ======================================
-    const database = client.db("ShipEx") ;
-    const parcelCollection = database.collection("percels") ;
-
+    const database = client.db("ShipEx");
+    const parcelCollection = database.collection("percels");
 
     // ===================== Parcel Related API ================================
 
     // ------------------- Api to get percel from Database ------------------------
-    app.get('/parcel',async(req,res)=>{
-        const query = {} ;
-        const email = req.query.email ;
-        if(email){
-            query.senderEmail = email ;
-        }
-        const cursor = await parcelCollection.find(query) ;
-        const result = await cursor.toArray() ;
-        res.send(result) ;
-    }) 
+    app.get("/parcel", async (req, res) => {
+      const query = {};
+      const email = req.query.email;
+      if (email) {
+        query.senderEmail = email;
+      }
+      const cursor = await parcelCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // ----------------------  APi to get Parcel By Id ----------------------
-    app.get('/parcel/:id' , async(req,res)=>{
-      const parcel_id = req.params.id ;
-      const query = {_id : new ObjectId(parcel_id)} ;
-      const result =await parcelCollection.findOne(query)
-      res.send(result) ;
-    })
+    app.get("/parcel/:id", async (req, res) => {
+      const parcel_id = req.params.id;
+      const query = { _id: new ObjectId(parcel_id) };
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    });
 
     // ----------------------  APi to delete Parcel By Id ----------------------
-    app.delete('/parcel/:id' , async(req,res)=>{
-      const parcel_id = req.params.id ;
-      const query = {_id : new ObjectId(parcel_id)} ;
-      const result = await parcelCollection.deleteOne(query) ;
-      res.send(result) ;
-    })
-    
-    // -------------------- Api to Add percel to Database --------------------------- 
-    app.post('/parcel',async(req,res)=>{
-        const newParcel = req.body ;
-        newParcel.createdAt = new Date() ;
-        const result =await parcelCollection.insertOne(newParcel) ;
-        res.send(result) ;
-    })
+    app.delete("/parcel/:id", async (req, res) => {
+      const parcel_id = req.params.id;
+      const query = { _id: new ObjectId(parcel_id) };
+      const result = await parcelCollection.deleteOne(query);
+      res.send(result);
+    });
 
+    // -------------------- Api to Add percel to Database ---------------------------
+    app.post("/parcel", async (req, res) => {
+      const newParcel = req.body;
+      newParcel.createdAt = new Date();
+      const result = await parcelCollection.insertOne(newParcel);
+      res.send(result);
+    });
+
+    // ------------- Stripe Payment Gateway ----------------------
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const parcelAmoumt = parseInt(paymentInfo.amount) * 100;
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              product_data: {
+                name:  `Payment for parcel ${paymentInfo.parcelname}`,
+              },
+              unit_amount: parcelAmoumt,
+              currency: "bdt",
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: 'payment',
+        metadata: {
+          parcel_id: paymentInfo.id,
+        },
+        success_url: `${process.env.STRIPE_DOMAIN}/paymentsuccess`,
+        cancel_url: `${process.env.STRIPE_DOMAIN}/paymentcancel`,
+      });
+
+      console.log(session);
+      res.send({ url: session.url });
+    });
 
     //----------------------- Reminder  -> Comment this Out when deploying to vercel -------------------
     await client.db("admin").command({ ping: 1 });
